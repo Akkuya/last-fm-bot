3cbb0a87b7a7086115f80d632143ef62b9d39bfe
Rishi <satyajitdatta@gmail.com>
2022-12-18 10:38:44 -0500

    added support for mongodb

--- src/bot.js
+++ src/bot.js
@@ -1,16 +1,13 @@
 require("dotenv").config();
 
-const { token } = process.env;
-const { 
-    Client, 
-    Collection, 
-    GatewayIntentBits 
-} = require("discord.js");
+const { token, databaseToken } = process.env;
+const { connect } = require(`mongoose`);
+const { Client, Collection, GatewayIntentBits } = require("discord.js");
 
 const fs = require("fs");
 
-const client = new Client({ 
-    intents: GatewayIntentBits.Guilds 
+const client = new Client({
+  intents: GatewayIntentBits.Guilds,
 });
 
 client.commands = new Collection();
@@ -20,14 +17,17 @@ client.colour = "";
 
 const functionFolders = fs.readdirSync(`./src/functions`);
 for (const folder of functionFolders) {
-    const functionFiles = fs
-        .readdirSync(`./src/functions/${folder}`)
-        .filter((file) => file.endsWith(".js"));
-    for (const file of functionFiles)
-        require(`./functions/${folder}/${file}`)(client);
+  const functionFiles = fs
+    .readdirSync(`./src/functions/${folder}`)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of functionFiles)
+    require(`./functions/${folder}/${file}`)(client);
 }
 
 client.handleEvents();
 client.handleCommands();
 client.handleComponents();
 client.login(token);
+(async () => {
+  await connect(databaseToken).catch(console.error);
+})();
